b48286f3f18d6e5876657a0ea560c1096b9094fc
Omar AlSaghier <oalsaghier@gmail.com>
2024-11-02 15:37:26 +0300

    and required file for week2 ETL hands-on

---
+++ week2/etl_pipelines/source_data/postgres_db/create_seed_data.sql
@@ -0,0 +1,15 @@
+-- Write the above to the previously created DB server
+
+create table if not exists src_table(
+	car_id serial PRIMARY KEY,
+	car_model VARCHAR(50),
+	year_of_manufacture INT,
+	price FLOAT8,
+	fuel VARCHAR(50),
+	year_of_selling DATE
+);
+
+COPY src_table(car_model,year_of_manufacture,price,fuel,year_of_selling)
+FROM '/data/src_data/src_data.csv'
+DELIMITER ','
+CSV HEADER;
